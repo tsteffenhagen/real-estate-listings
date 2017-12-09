3a98764d82d786e6f7cc0f4d853db3ecdbf8b805
Tyler Steffenhagen <steffenhagen.tyler@gmail.com>
2017-12-08 18:00:59 -0600

    server is now listening on 5000

---
+++ server/server.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+
+var mongooseConnection = ('./modules/mongoose-connection');
+var listing = require('./routes/listing.js');
+var rental = require('./routes/rental');
+
+var app = express();
+var port = process.env.PORT || 5000;
+
+app.use(bodyParser.json());
+app.use(express.static('server/public'));
+mongooseConnection.connect();
+
+app.use('/listing', listing);
+app.use('/rental', rental);
+
+app.listen(port, function() {
+    console.log('listening on port', port);
+    
+})
